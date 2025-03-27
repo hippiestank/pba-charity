bb2b335d30ae407b8bb670c36baa48dde469993c
miquelcabot <miquel.cabot@gmail.com>
2025-03-27 17:23:12 +0100

    add Charity contract for managing donations and charity information

---
+++ contracts/Charity.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+contract PbaCharity {
+  struct Charity {
+    string name;
+    string description;
+  }
+
+  mapping(address => Charity) public charities;
+  mapping(address => uint256) public tokenBalances;
+
+  constructor() {
+    
+  }
+
+  function addCharity(string memory name, string memory description) public {
+    charities[msg.sender] = Charity(name, description);
+  }
+
+  function donate(address charity, address token, uint256 amount) public payable {
+    require(
+      keccak256(abi.encodePacked(charities[charity].name)) != keccak256(abi.encodePacked("")),
+      "Charity does not exist"
+    );
+    require(amount > 0, "Amount must be greater than 0");
+
+    if (token != address(0)) {
+      // Transfer tokens
+      IERC20(token).transferFrom(msg.sender, charity, amount);
+    } else {
+      // Transfer ether
+      tokenBalances[charity] += msg.value;
+    }
+  }
+}
